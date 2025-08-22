aa687a227aa1fcc1edbc0214b0fcb678afa77da5
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-22 14:38:05 -0500

    feature: Preparacion - Subir Estructura de Paquetes del Laboratorio

---
+++ src/main/java/edu/dosw/lab/Application.java
@@ -0,0 +1,8 @@
+package main.edu.dosw.lab;
+
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
