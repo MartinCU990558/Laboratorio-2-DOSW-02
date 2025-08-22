e8235ed3b755eda442b98ebd877b2d7b9c763e91
daniel.palacios-m <daniel.palacios-m@mail.escuelaing.edu.co>
2025-08-22 17:18:16 -0500

    feature: creacion clase ShoppingCart y desarrollo del main

--- src/main/java/edu/dosw/lab/creacionales/reto1/Reto1.java
+++ src/main/java/edu/dosw/lab/creacionales/reto1/Reto1.java
@@ -2,6 +2,20 @@ package edu.dosw.lab.creacionales.reto1;
 
 public class Reto1 {
     public static void main(String[] args) {
+        System.out.println("Bienvenido a la tienda Don Pepe!\n");
 
+        Product camiseta = new Camiseta();
+        Product pantalon = new Pantalon();
+        Product galletas = new Galletas();
+        Product jugo = new JugoNatural();
+
+        Customer cliente = new FrequentCustomer("Frecuente");
+
+        ShoppingCart carrito = new ShoppingCart(cliente);
+        carrito.addItem(camiseta, 2);
+        carrito.addItem(galletas, 3);
+        carrito.addItem(jugo, 5);
+
+        carrito.printReceipt();
     }
 }
---
+++ src/main/java/edu/dosw/lab/creacionales/reto1/ShoppingCart.java
@@ -0,0 +1,36 @@
+package edu.dosw.lab.creacionales.reto1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private Customer customer;
+    private List<CartItem> items;
+
+    public ShoppingCart(Customer customer) {
+        this.customer = customer;
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Product product, int quantity) {
+        items.add(new CartItem(product, quantity));
+        System.out.println(product.getName() + " " + quantity + " unidades agregado al carrito.");
+    }
+
+    public void printReceipt() {
+        System.out.println("\n--- RECIBO DE COMPRA ---");
+        System.out.println("Cliente: " + customer.getName());
+        double subtotal = 0;
+        for (CartItem item : items) {
+            double total = item.getTotalPrice();
+            subtotal += total;
+            System.out.println("- " + item.getProduct().getName() + " - $" + (int)total);
+        }
+        double discount = subtotal * customer.getDiscountRate();
+        double total = subtotal - discount;
+        System.out.println("\nSubtotal: $" + (int)subtotal);
+        System.out.println("Descuento aplicado: $" + (int)discount);
+        System.out.println("Total a pagar: $" + (int)total);
+        System.out.println("------------------------");
+        System.out.println("¡Gracias por su compra!");
+    }
+}
