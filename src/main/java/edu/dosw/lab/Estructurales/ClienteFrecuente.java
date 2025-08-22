ea5f7c5378470ff929d473934814b87598e5de8d
unknown <daniel.rodriguez-b@labinfo.is.escuelaing.edu.co>
2025-08-22 17:59:54 -0500

    Corrreccion estructura de la clase Cliente junto a sus variantes

--- src/main/java/edu/dosw/lab/Estructurales/ClienteFrecuente.java
+++ src/main/java/edu/dosw/lab/Estructurales/ClienteFrecuente.java
@@ -1,10 +1,5 @@
 package edu.dosw.lab.Estructurales;
 public class ClienteFrecuente extends Cliente {
-
-
-    public ClienteFrecuente(String nombre) {
-        super("Frecuente");
-    }
     @Override
     public double calcularDescuento(double compraFinal) {
         return compraFinal * 0.1;
--- src/main/java/edu/dosw/lab/Estructurales/ClienteNuevo.java
+++ src/main/java/edu/dosw/lab/Estructurales/ClienteNuevo.java
@@ -1,10 +1,6 @@
 package edu.dosw.lab.Estructurales;
 
 public class ClienteNuevo extends Cliente {
-
-    public ClienteNuevo(String nombre) {
-        super("Nuevo");
-    }
     @Override
     public double calcularDescuento(double compraFinal) {
         return compraFinal * 0.05;}
