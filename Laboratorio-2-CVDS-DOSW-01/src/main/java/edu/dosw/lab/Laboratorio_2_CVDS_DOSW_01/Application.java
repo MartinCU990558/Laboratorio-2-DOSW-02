5122ca3477599770b68661f6ca52314be93508df
mbquial <pepitotolitis@gmail.com>
2025-08-21 21:49:05 -0500

    feature: Preparación - Subir Estructura de Paquetes del Laboratorio 02

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
@@ -0,0 +1,9 @@
+package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
+	}
+
+}
