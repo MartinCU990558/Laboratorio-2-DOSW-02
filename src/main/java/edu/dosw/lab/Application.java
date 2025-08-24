e9fb1c19e1d0071fdefb698645032fb874f18b77
Sofia <Sofia>
2025-08-24 16:03:44 -0500

    add run to aplication

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -4,7 +4,6 @@ import main.java.edu.dosw.lab.estructurales.reto5.reto5;
 
 public class Application {
     public static void main(String[] args){
-        System.out.println("Proyecto Maven configurado y corriendo correctamente");
         reto5.run();
     }
 }
