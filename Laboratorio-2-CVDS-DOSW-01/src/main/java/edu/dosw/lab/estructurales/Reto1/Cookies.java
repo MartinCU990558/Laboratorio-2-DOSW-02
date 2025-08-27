f780f7e37184dab268d3c7531b65837a53ff54c5
Maria Belén <maria.quintero-a@mail.escuelaing.edu.co>
2025-08-27 16:17:29 -0500

    primer commit del reto 1

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/estructurales/Reto1/Cookies.java
@@ -0,0 +1,6 @@
+public class Cookies implements Product {
+    @Override
+    public String getName() { return "Galletas"; }
+    @Override
+    public int getPrice() { return 500; }
+}
---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/estructurales/Reto1/Pants.java
@@ -0,0 +1,6 @@
+public class Pants implements Product {
+    @Override
+    public String getName() { return "Pantalón"; }
+    @Override
+    public int getPrice() { return 50000; }
+}
