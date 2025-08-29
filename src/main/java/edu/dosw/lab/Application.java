5499d432fe41922d369ec54d4bb17d8d609016b2
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-08-28 22:55:39 -0500

    Feature: aplicación usando maven reto 4, completo

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,5 +1,6 @@
 package edu.dosw.lab;
 
+import edu.dosw.lab.comportamiento.reto4.Reto4;
 import edu.dosw.lab.creacionales.reto1.Reto1;
 import edu.dosw.lab.creacionales.reto3.Reto3;
 import edu.dosw.lab.estructurales.reto2.Reto2;
@@ -7,8 +8,9 @@ import edu.dosw.lab.estructurales.reto2.Reto2;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Proyecto Maven configurado y corriendo correctamente");
-        //Reto1.ejecutar();
-        //Reto2.main(args);
-        Reto3.main(args);
+        // Reto1.ejecutar();
+        // Reto2.main(args);
+        // Reto3.main(args);
+        Reto4.main(args);
     }
 }
