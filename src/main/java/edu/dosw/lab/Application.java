807a69092364aa1682873cd896062af755dd5027
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-08-29 09:34:48 -0500

    feature: prueba exitosa usando maven y evidencia.

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -2,6 +2,7 @@ package edu.dosw.lab;
 
 import edu.dosw.lab.comportamiento.reto4.Reto4;
 import edu.dosw.lab.comportamiento.reto6.Reto6;
+import edu.dosw.lab.comportamiento.reto7.Reto7;
 import edu.dosw.lab.creacionales.reto1.Reto1;
 import edu.dosw.lab.creacionales.reto3.Reto3;
 import edu.dosw.lab.estructurales.reto2.Reto2;
@@ -15,6 +16,7 @@ public class Application {
         // Reto3.main(args);
         //Reto4.main(args);
         //Reto5.main(args);
-        Reto6.main(args);
+        //Reto6.main(args);
+        Reto7.main(args);
     }
 }
