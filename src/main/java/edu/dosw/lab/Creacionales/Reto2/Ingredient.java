1cae54603d16d5f74ec9525e68781ac8bad3b841
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-28 21:42:45 -0500

    clase ingrediente

---
+++ src/main/java/edu/dosw/lab/Creacionales/Reto2/Ingredient.java
@@ -0,0 +1,25 @@
+package edu.dosw.lab.Creacionales.Reto2;
+
+public class Ingredient {
+    private String name;
+    private double price;
+
+    public Ingredient(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+ 
