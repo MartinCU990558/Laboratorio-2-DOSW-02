8abdfc80e83a41223fd670b08e0e16e026a96b20
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-08-29 09:12:44 -0500

    feature: aplicación reto 6

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,6 +1,7 @@
 package edu.dosw.lab;
 
 import edu.dosw.lab.comportamiento.reto4.Reto4;
+import edu.dosw.lab.comportamiento.reto6.Reto6;
 import edu.dosw.lab.creacionales.reto1.Reto1;
 import edu.dosw.lab.creacionales.reto3.Reto3;
 import edu.dosw.lab.estructurales.reto2.Reto2;
@@ -13,6 +14,7 @@ public class Application {
         // Reto2.main(args);
         // Reto3.main(args);
         //Reto4.main(args);
-        Reto5.main(args);
+        //Reto5.main(args);
+        Reto6.main(args);
     }
 }
