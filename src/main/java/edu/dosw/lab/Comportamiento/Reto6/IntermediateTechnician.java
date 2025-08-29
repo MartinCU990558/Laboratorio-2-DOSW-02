86d4bf5a472c1175b2a9450281d3f650bbcf0886
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-28 23:02:20 -0500

    se corrigieron paquetes

--- src/main/java/edu/dosw/lab/Comportamiento/Reto6/IntermediateTechnician.java
+++ src/main/java/edu/dosw/lab/Comportamiento/Reto6/IntermediateTechnician.java
@@ -1,4 +1,4 @@
-package edu.dosw.lab.Comportamiento.reto6;
+package edu.dosw.lab.Comportamiento.Reto6;
 
 public class IntermediateTechnician extends Technician {
     public IntermediateTechnician() {
