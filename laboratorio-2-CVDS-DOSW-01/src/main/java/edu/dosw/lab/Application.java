958f2fd5e8cbc7dcbf3baa19566369dc65e31760
MAGG0059 <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-08-29 00:32:03 -0500

    application reto1

--- laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Application.java
+++ laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Application.java
@@ -2,9 +2,11 @@ package edu.dosw.lab;
 
 import edu.dosw.lab.creacionales.reto3.Reto3;
 import edu.dosw.lab.estructurales.Reto2.Reto2;
+import edu.dosw.lab.estructurales.Reto1.reto1;
 import edu.dosw.lab.comportamientos.Reto4.Reto4;
 
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("Proyecto de Maven configurado y ejecutado correctamente");
         Reto3.iniciarPrograma();
